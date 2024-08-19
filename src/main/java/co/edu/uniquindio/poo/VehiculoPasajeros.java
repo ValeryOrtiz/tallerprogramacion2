3081373cf48baba0dceecfa42cd18e551917dc35
ValeryOrtiz <valery.comortiz@gmail.com>
2024-08-19 00:32:04 -0500

    usuario

--- src/main/java/co/edu/uniquindio/poo/VehiculoPasajeros.java
+++ src/main/java/co/edu/uniquindio/poo/VehiculoPasajeros.java
@@ -1,14 +1,22 @@
 package co.edu.uniquindio.poo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class VehiculoPasajeros extends Vehiculo {
     private int maximoPasajeros;
+    private List<Usuario> usuarios;
     
 
     public VehiculoPasajeros(String placa, String modelo, String marca, String color, String numeroChasis, int maximoPasajeros) {
         super(placa, modelo, marca, color,numeroChasis);
         this.maximoPasajeros = maximoPasajeros;
+        this.usuarios = new ArrayList<>();
     }
     
+    public void agregarUsuario(Usuario usuario){
+        this.usuarios.add(usuario);
+    }
 
     public int getMaximoPasajeros() {
         return maximoPasajeros;
@@ -25,7 +33,12 @@ public class VehiculoPasajeros extends Vehiculo {
                 + getModelo() + ", Marca=" + getMarca() + ", Color=" + getColor() + ", numero de chasis=" + getNumeroChasis() + "]";
     }
 
-    
+    public List<Usuario> getUsuarios() {
+        return usuarios;
+    }
 
-    
+    public void setUsuarios(List<Usuario> usuarios) {
+        this.usuarios = usuarios;
+    }
+
 }
